79a8573118f3cfad12cf8b10fe9931020077e226
mimosafa <mimosafa@gmail.com>
2020-06-27 22:19:52 +0900

    add /car-rental/quote route

---
+++ src/pages/car-rental/quote/quote.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h2>キッチンカーレンタル かんたんお見積り</h2>
+  </div>
+</template>
--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,10 +1,15 @@
 import Root from './index.vue';
+import CarRentalQuote from './car-rental/quote/quote.vue';
 import NotFound from './404.vue';
 
 export default [
   {
     path: '/',
     component: Root
+  },
+  {
+    path: '/car-rental/quote',
+    component: CarRentalQuote
   },
   {
     path: '*',
