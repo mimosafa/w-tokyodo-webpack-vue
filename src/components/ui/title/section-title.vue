6f57926d14d175fa2d3b30ade9eeebf01d650e68
mimosafa <mimosafa@gmail.com>
2020-07-15 12:38:03 +0900

    add title ui component

---
+++ src/components/ui/title/section-title.vue
@@ -0,0 +1,51 @@
+<template>
+  <h3 :class="color ? `color-${color}` : ''"><slot /></h3>
+</template>
+
+<script>
+export default {
+  props: {
+    color: String
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+h3 {
+  position: relative;
+  text-align: center;
+
+  @include phone {
+    padding-bottom: 15px;
+    margin-bottom: 20px;
+  }
+  @include desktop {
+    padding-bottom: 20px;
+    margin-bottom: 35px;
+  }
+
+  &:after {
+    content: "";
+    background-color: #333;
+    display: block;
+    position: absolute;
+    left: 50%;
+    bottom: 0;
+
+    @include phone {
+      width: 40px;
+      height: 2px;
+      margin-left: -20px;
+    }
+    @include desktop {
+      width: 60px;
+      height: 2px;
+      margin-left: -30px;
+    }
+  }
+
+  &.color-orange:after {
+    background-color: $orange;
+  }
+}
+</style>
