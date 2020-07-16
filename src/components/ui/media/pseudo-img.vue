9568e65b58f02255612749e154a57e1eb53930c8
mimosafa <mimosafa@gmail.com>
2020-07-16 10:05:19 +0900

    add pseudo image ui component

---
+++ src/components/ui/media/pseudo-img.vue
@@ -0,0 +1,30 @@
+<template>
+  <span :class="imgClass" :style="imgStyle"></span>
+</template>
+
+<script>
+export default {
+  props: {
+    src: String
+  },
+  computed: {
+    imgStyle() {
+      return this.src ? `background-image: url(${this.src});` : '';
+    },
+    imgClass() {
+      return this.src ? 'hasimg' : 'noimg';
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.hasimg {
+  display: inline-block;
+  background: {
+    repeat: no-repeat;
+    size: cover;
+    position: 50% 50%;
+  }
+}
+</style>
